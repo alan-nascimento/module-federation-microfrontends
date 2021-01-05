9a53380d713178c21f6be54163d3a685e5858b3b
Alan Nascimento <alan.silva.n@hotmail.com>
2021-01-05 00:21:05 -0300

    chore: wiring up react

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (element) => {
+  ReactDOM.render(<h1>Hi there</h1>, element);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.getElementById('_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
