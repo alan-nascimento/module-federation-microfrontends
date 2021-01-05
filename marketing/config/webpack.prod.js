3e764d99b650f98ad94c498d4e1878dceaf7e22c
Alan Nascimento <alan.silva.n@hotmail.com>
2021-01-05 00:01:41 -0300

    chore: setup babel

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -3,6 +3,14 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
       },
     ],
   },
