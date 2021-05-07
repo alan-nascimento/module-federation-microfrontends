5a142e0a74da2c049a28674f31deb1235109303a
Alan Nascimento <alan.silva.n@hotmail.com>
2021-05-07 17:31:29 -0300

    ci: trigger auth

--- modules/auth/src/pages/signup.js
+++ modules/auth/src/pages/signup.js
@@ -49,7 +49,7 @@ const SignUp = ({ onSignIn }) => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign up
+          Sign up!
         </Typography>
         <form
           onSubmit={(e) => e.preventDefault()}
